0481a60f6acc510c37022b2d85f24b60209f9e65
rlilja10 <rlilja10@gmail.com>
2017-07-03 16:27:49 -0400

    Add files via upload
    commented out some unnecessary lines and fixed a typo

--- NotSoMinority_BefTuning_MySQL.sql
+++ NotSoMinority_BefTuning_MySQL.sql
@@ -1,5 +1,4 @@
 --SQL queries before tuning and before adding any primary or foreign keys
---studName in Student is used to refer to the student's name
 USE Project3;
 --1.List the name of the student with id equal to v1 (id).
 --Takes 0.00 to 0.01 sec to execute
@@ -7,12 +6,12 @@ SELECT name FROM Student WHERE id = 998983;
 
 --2.List the names of students with id in the range of v2 (id) to v3 (inclusive).
 --Takes 0.00 to 0.01 sec to execute
-SELECT name FROM Student WHERE id >= 927358 AND id <= 998442;
+SELECT name FROM Student WHERE id >= 736096 AND id <= 998442;
 
 --Finds all the students whose ids match from Transcript and have taken courses
 --Takes 1.95 to 2.49 sec to execute
-SELECT Student.name, Student.id, Transcript.crsCode FROM Student, Transcript 
-WHERE Student.id = Transcript.studId;
+--SELECT Student.name, Student.id, Transcript.crsCode FROM Student, Transcript 
+--WHERE Student.id = Transcript.studId;
 --3.List the names of students who have taken course v4 (crsCode).
 --Takes 0.01 to 0.02 sec to execute
 SELECT Student.name FROM Student, Transcript  WHERE Student.id = 
@@ -20,18 +19,18 @@ Transcript.studId AND Transcript.crsCode = 'crsCode579449';
 
 --Returns all professors teaching a course
 --Takes 2.67 to 2.81 sec to execute
-SELECT Professor.name, Teaching.crsCode FROM Professor, Teaching WHERE 
-Professor.id = Teaching.profId;
+--SELECT Professor.name, Teaching.crsCode FROM Professor, Teaching WHERE 
+--Professor.id = Teaching.profId;
 --4.List the names of students who have taken a course taught by professor v5 (name).
 --Takes 17.78 to 18.70 sec to execute
 SELECT Student.name, Transcript.crsCode FROM Student,  Transcript, Teaching, Professor 
-HERE Student.id = Transcript.studId AND  Professor.id = Teaching.profId AND 
+WHERE Student.id = Transcript.studId AND  Professor.id = Teaching.profId AND 
 Professor.name = 'name260723';
 
 --Finds all the students who take courses from any department
 --Takes 4.38 to 4.43 sec to execute
-SELECT Student.name, Transcript.crsCode, Course.deptId FROM Student, Course, 
-Transcript WHERE Student.id = Transcript.studId AND Course.crsCode = Transcript.crsCode;
+--SELECT Student.name, Transcript.crsCode, Course.deptId FROM Student, Course, 
+--Transcript WHERE Student.id = Transcript.studId AND Course.crsCode = Transcript.crsCode;
 --5.List the names of students who have taken a course from department v6 (deptId), 
 --but not v7.
 --Takes 0.01 to 0.02 sec to execute
