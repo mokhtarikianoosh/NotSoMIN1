bd2c0c4c6ca8c9181ea55a2ef5eeea17fc224fce
rlilja10 <rlilja10@gmail.com>
2017-07-05 15:34:13 -0400

    Add files via upload
    Reflects the addition of another index.

--- NotSoMinority_Project3_After_MySQL.sql
+++ NotSoMinority_Project3_After_MySQL.sql
@@ -28,7 +28,8 @@ CREATE TABLE `Course` (
   `crsName` varchar(255) DEFAULT NULL,
   `descr` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`crsCode`),
-  KEY `courseName` (`crsName`)
+  KEY `courseName` (`crsName`),
+  KEY `departmentId` (`deptId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -161,4 +162,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-05 13:09:49
+-- Dump completed on 2017-07-05 15:33:31
